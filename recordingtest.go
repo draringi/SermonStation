3e16f2db5b332e44418ad2a5359e1ccdc79f0189
Michael Williams <draringi@draringi.net>
2016-01-31 10:56:38 -0500

    Added test app

---
+++ recordingtest.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"./audio"
+	"code.google.com/p/portaudio-go/portaudio"
+	"fmt"
+	"os"
+	"os/signal"
+)
+
+func getDevice() *portaudio.DeviceInfo {
+	hs, err := portaudio.HostApis()
+	if err != nil {
+		panic(err)
+	}
+	for {
+		fmt.Println("Please Select an API")
+		for index, api = range hs {
+			fmt.Printf("%d: %s\n", index, api.Name)
+		}
+		var selection int
+		fmt.Printf("API #: ")
+		fmt.Scanf("%d", &selection)
+		api := hs[selection]
+		fmt.Println("Please select an input device")
+		for index, dev = range hs.Devices {
+			fmt.Printf("%d: %s (%d channels)\n", index, dev.Name, dev.MaxInputChannels)
+		}
+		fmt.Printf("Device #: ")
+		fmt.Scanf("%d", &selection)
+		dev := ds.Devices[selection]
+		return dev
+	}
+}
+
+func getSampleSize() int {
+	for {
+		var sampleSize int
+		fmt.Printf("How big should a sample be? {32, 24, 16, 8} ")
+		fmt.Scanf("%d", &sampleSize)
+		switch sampleSize {
+		case 8, 16, 24, 32:
+			return sampleSize
+		default:
+			fmt.Println("Invalid Sample Size. Must be 32, 24, 16 or 8")
+		}
+	}
+}
+
+func main() {
+	device := getDevice()
+	fmt.Printf("Recording with %d\n", device.Name)
+	var chanCount, sampleSize int
+	for {
+		fmt.Printf("How many channels? [1,%d] ", device.MaxInputChannels)
+		fmt.Scanf("%d", &chanCount)
+		if chanCount > 0 && chanCount <= dev.device.MaxInputChannels {
+			break
+		} else {
+			fmt.Println("Invalid number of channels")
+		}
+	}
+	sampleSize = getSampleSize()
+	var path string
+	fmt.Println("Where should the audio be saved to?")
+	fmt.Scanf("%s", &path)
+	var params portaudio.StreamParameters
+	var devParams portaudio.StreamDeviceParameters
+	devParams.Channels = chanCount
+	devParams.Device = dev
+	devParams.Latency = dev.DefaultLowInputLatency
+	params.SampleRate = dev.DefaultSampleRate
+	params.FramesPerBuffer = 128
+	params.Input = devParams
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+	recorder = audio.NewRecording(path, params, chanCount, sampleSize)
+	fmt.Println("Starting Recording")
+	recorder.start()
+	fmt.Println("Recording")
+	for {
+		switch {
+		case <-sig:
+			recorder.Stop()
+		case recorder.Status() == audio.STOPPED:
+			fmt.Println("Recording Stopped")
+			return
+		}
+	}
+}
